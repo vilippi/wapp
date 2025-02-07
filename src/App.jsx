062756d7a406519679c59311ccfcd78b57bd441a
Vitor Lippi <vitorlippilima.vl@gmail.com>
2025-02-07 13:27:15 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+const App = () =>
+<>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+
+
+export default App
